79d92340546b4dd3a5bb2c397cb42dc3980df014
eduardorcury <eduardoribeirocury@gmail.com>
2020-06-24 11:50:34 -0400

    Atualização do usuário com PUT

--- src/main/java/com/eduardorib/resources/UserResource.java
+++ src/main/java/com/eduardorib/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,8 +44,8 @@ public class UserResource {
 				
 	}
 	
-	@PostMapping()
-	public ResponseEntity<Void> INSERT(@RequestBody UserDTO objDTO) {
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
 		
 		User obj = userService.fromDTO(objDTO);
 		obj = userService.insert(obj);
@@ -58,6 +59,16 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		
 		userService.delete(id);
+		return ResponseEntity.noContent().build();
+				
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
+		
+		User obj = userService.fromDTO(objDTO);
+		obj.setId(id);
+		obj = userService.update(obj);
 		return ResponseEntity.noContent().build();
 				
 	}
--- src/main/java/com/eduardorib/services/UserService.java
+++ src/main/java/com/eduardorib/services/UserService.java
@@ -13,29 +13,42 @@ import com.eduardorib.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		
+
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		userRepository.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return userRepository.save(newObj);
+
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}

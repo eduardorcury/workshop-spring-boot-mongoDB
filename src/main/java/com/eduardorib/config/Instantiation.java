30091bd29423ac11491f1d3df5ac17cd24321bc1
eduardorcury <eduardoribeirocury@gmail.com>
2020-06-24 12:27:33 -0400

    Dados do autor com AuthorDTO

--- src/main/java/com/eduardorib/config/Instantiation.java
+++ src/main/java/com/eduardorib/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.eduardorib.domain.Post;
 import com.eduardorib.domain.User;
+import com.eduardorib.dto.AuthorDTO;
 import com.eduardorib.repository.PostRepository;
 import com.eduardorib.repository.UserRepository;
 
@@ -18,13 +19,13 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
@@ -34,11 +35,13 @@ public class Instantiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Viajando para São Paulo, abçs", maria);
-		Post post2 = new Post(null, sdf.parse("24/06/2020"), "Bom dia", "Estudando mongoDB", maria);
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Viajando para São Paulo, abçs",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("24/06/2020"), "Bom dia", "Estudando mongoDB", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}

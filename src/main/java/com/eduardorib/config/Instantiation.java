a46bac3856ee8086355d08f7e2f6419195e288b7
eduardorcury <eduardoribeirocury@gmail.com>
2020-06-24 12:20:52 -0400

    Criando Entity Post com user aninhado no banco de dados

--- src/main/java/com/eduardorib/config/Instantiation.java
+++ src/main/java/com/eduardorib/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.eduardorib.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.eduardorib.domain.Post;
 import com.eduardorib.domain.User;
+import com.eduardorib.repository.PostRepository;
 import com.eduardorib.repository.UserRepository;
 
 @Configuration
@@ -14,17 +18,28 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2020"), "Partiu viagem", "Viajando para São Paulo, abçs", maria);
+		Post post2 = new Post(null, sdf.parse("24/06/2020"), "Bom dia", "Estudando mongoDB", maria);
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
 
---
+++ src/main/java/com/eduardorib/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.eduardorib.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eduardorib.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+	
+
+}

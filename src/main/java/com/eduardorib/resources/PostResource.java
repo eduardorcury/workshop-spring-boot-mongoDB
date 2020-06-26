59cf5284d9a0c4fbdd2e3c3adb087c3a0e07d229
eduardorcury <eduardoribeirocury@gmail.com>
2020-06-26 12:29:50 -0400

    Consulta com query metods do Spring Data

--- src/main/java/com/eduardorib/resources/PostResource.java
+++ src/main/java/com/eduardorib/resources/PostResource.java
@@ -1,33 +1,41 @@
 package com.eduardorib.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eduardorib.domain.Post;
+import com.eduardorib.resources.util.URL;
 import com.eduardorib.services.PostService;
 
 @RestController
 @RequestMapping(value = "/posts")
 public class PostResource {
-	
+
 	@Autowired
 	private PostService postService;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
-		
+
 		Post obj = postService.findById(id);
 		return ResponseEntity.ok().body(obj);
-				
+
 	}
-	
+
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+
+	}
+
 }
-
-
-
-
-

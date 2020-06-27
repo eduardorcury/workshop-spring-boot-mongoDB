15de860bee9ed72d8b930b6ca3e1f74e1a3e939a
eduardorcury <eduardoribeirocury@gmail.com>
2020-06-26 22:26:23 -0400

    Consulta com vários critérios

--- src/main/java/com/eduardorib/services/PostService.java
+++ src/main/java/com/eduardorib/services/PostService.java
@@ -1,5 +1,6 @@
 package com.eduardorib.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,18 +16,25 @@ public class PostService {
 
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	public Post findById(String id) {
-		
+
 		Optional<Post> user = postRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 
 	}
-	
+
 	public List<Post> findByTitle(String text) {
-		
+
 		return postRepository.findByTitleContainingIgnoreCase(text);
-		
+
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
+
 	}
 
 }
